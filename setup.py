262c62ebf2203019f3666541cf9a9964fe66790f
Marco Pattaro <marco.pattaro@simplyopen.org>
2016-04-29 12:38:45 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from admin_reports import __version__
+setup(
+    name='admin_reports',
+    version=__version__,
+    description='Reports for django-admin',
+    long_description="Easily define and show data analysis reports for django-admin.",
+    author='Simplyopen',
+    author_email='info@simplyopen.org',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+    ],
+    packages=find_packages(),
+    package_dir={'admin_reports': 'admin_reports'},
+    install_requires=['Django>=1.7'],
+)
