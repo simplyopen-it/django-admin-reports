da0ddfbb80f3d172f6f8fbb831b415db34237e61
Marco Pattaro <marco.pattaro@simplyopen.org>
2017-10-04 11:32:48 +0200

    rev 0.10.8

--- admin_reports/__init__.py
+++ admin_reports/__init__.py
@@ -3,7 +3,7 @@ from .decorators import register
 from .sites import AdminReportSite, site
 from .reports import Report
 
-__version__ = '0.10.7'
+__version__ = '0.10.8'
 __all__ = ["register", "AdminReportSite", "site", "Report"]
 
 
