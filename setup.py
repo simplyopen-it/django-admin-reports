bc71905b4c8845069f639fc21be87008fe3ca27f
Marco Pattaro <marco.pattaro@simplyopen.org>
2020-11-11 11:28:14 +0100

    update Python versions in setup.py

--- setup.py
+++ setup.py
@@ -17,6 +17,10 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Internet :: WWW/HTTP :: WSGI :: Application'
     ],
     packages=find_packages(),
